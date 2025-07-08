45343872c1f8121adfc5cc33a234c9e2b0388c34
MurrayDesnoyer <murray.desnoyer@originintl.com>
2025-07-08 13:26:45 -0400

    Updated to support companyname and sandbox mode

--- server.js
+++ server.js
@@ -7,8 +7,10 @@ const express = require('express');
 
 const app = express();
 const path = require('path');
+
 app.use(express.static('public'));
 
+const PORT = process.env.PORT || 4242
 
 // // uses to storage user in addUser.js
 const bodyParser = require('body-parser');
@@ -16,37 +18,54 @@ const bodyParser = require('body-parser');
 // const getUser = require('./public/getUser');
 //------
 
-
-// --- used for nademailer
+// --- used for nodemailer
 const nodemailer = require('nodemailer')
 const ejs = require('ejs')
 
-// create the mail transportor for production
-const transporter = nodemailer.createTransport({
-  host: "live.smtp.mailtrap.io",
-  port: 587,                      // non encrypted port
-  secure: false,                  // upgrade later with STARTTLS
-  auth: {
-    user: "smtp@mailtrap.io",    //process.env.MAIL_ORIGIN_USER,
-    pass: process.env.MAIL_ORIGIN_PASS,
-  },
-  tls:{
-    rejectUnauthorized: false // Optional: allows self-signed certs (not recommended for production)
+const envLive = process.env.NODE_ENV_TEST;    // this environment varriable allows 
+console.log(envLive);
+
+// Note: for some reason the if structure prevents the transporter form being created
+//       which forces to use commenting out either the live or test transporters.
+// if (envLive === `live`){
+//   // create the mail transportor for production
+//   console.log(`using the live transporter`)
+//   const  transporter = nodemailer.createTransport({
+//     host: "live.smtp.mailtrap.io",
+//     port: 587,                      // non encrypted port
+//     secure: false,                  // upgrade later with STARTTLS
+//     auth: {
+//       user: "smtp@mailtrap.io",    //process.env.MAIL_ORIGIN_USER,
+//       pass: process.env.MAIL_ORIGIN_PASS,
+//     },
+//     tls:{
+//       rejectUnauthorized: false // Optional: allows self-signed certs (not recommended for production)
+//     }
+//   });
+// } else {
+// create the mail transporter function for testing
+  console.log(`using the sandbox transporter`)
+  const transporter = nodemailer.createTransport({
+    host: "sandbox.smtp.mailtrap.io",
+    port: 2525,
+    //secure: false, // upgrade later with STARTTLS
+    auth: {
+      user: process.env.MAIL_SANDBOX_USER,
+      pass: process.env.MAIL_SANDBOX_PASS,
+    }
+  });
+//};
+
+// Optional: verify connection configuration
+transporter.verify(function (error, success) {
+  if (error) {
+    console.log('Error:', error);
+  } else {
+    console.log('Server is ready to take our messages');
   }
 });
 
-//---------------------------------------------
-
-// // create the mail transporter function for testing
-// const transporter = nodemailer.createTransport({
-//   host: "sandbox.smtp.mailtrap.io",
-//   port: 2525,
-//   //secure: false, // upgrade later with STARTTLS
-//   auth: {
-//     user: process.env.MAIL_SANDBOX_USER,
-//     pass: process.env.MAIL_SANDBOX_PASS,
-//   }
-// });
+// ------------------------------------------------
 
 // define  the send mail function
 async function sendEmail(to, subject, template, data) {
@@ -83,7 +102,6 @@ app.use(bodyParser.json());
 // const express = require('express');   // defined above
 // const app = express();                  // defined above
  
-//app.use(express.json()); 
 const userStack = [];
 
 //---------------
@@ -91,61 +109,57 @@ const userStack = [];
 // main entry point to link to Stripe checkout -------------------------------------
 app.post('/create-checkout-session', async (req, res) => {
 
-  console.log('enter postcreate checkout')
+      console.log('enter postcreate checkout')
+    
+    //--- jane19 -- extract from form created with the submit button see Submit.html 
+    // in production will have to be from hubspot.  ?? may need the use of keys / authentication. 
+      const { username, email, companyname } = req.body;
+      console.log('Form Data:', req.body);
 
- 
-//--- jane19 -- extract from form created with the submit button see Submit.html 
-// in production will have to be from hubspot.  ?? may need the use of keys / authentication. 
-  const { username, email } = req.body;
-  console.log('Form Data:', req.body);
+      //const { userName, email } = req.body; 
+      userStack.push({ username, email , companyname }); 
+      console.log('stack name: ', username, 'email:',email, `Company:`,companyname)
+      //res.send('User pushed to stack'); 
+    
+      //res.send(`Received submission: ${username} (${email})`);
+      console.log("after app.get")
+      console.log("UserName:",username)
+      console.log("Email:",email)
 
-  //const { userName, email } = req.body; 
-  userStack.push({ username, email }); 
-  console.log('stack name: ', username, 'email:',email)
-  //res.send('User pushed to stack'); 
- 
+    //--- june 19
 
+      //const sessionId = req.body.sessionId;
+      const session = await stripe.checkout.sessions.create({
+        //customer_email:'murraydesnoyer@hotmail.com',    // not substutite this with the email varriable from the HTTP push URL.
+      
+            customer_email:email,
+        line_items: [
+          {
+            
+            // Provide the exact Price ID (for example, price_1234) of the product you want to sell price_1REaqoJz9K1DGkoKdW8pTe0F
+            price: 'price_1REakMJz9K1DGkoKFgvTbLat',
+            quantity: 1,
+          },
+        ],
+        mode: 'payment',
+        // success_url: `${YOUR_DOMAIN}/success.html`,  
+        // cancel_url: `${YOUR_DOMAIN}/cancel.html`,
+        // substute for production  note sessionID will enable us to get infor about the transaction. 
+        //success_url: `${process.env.BASE_URL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
+        //success_url: `${process.env.BASE_URL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
+        //cancel_url: `${process.env.BASE_URL}/cancel.html`,
+        success_url: `${process.env.BASE_URL}/complete?session_id={CHECKOUT_SESSION_ID}`,
+        cancel_url: `${process.env.BASE_URL}/cancel?session_id={CHECKOUT_SESSION_ID}`,
 
+        automatic_tax: {enabled: true},
+      });
+      console.log("Post await stripe checkout")
+      console.log("Email:",email)
+      //console.log(session)
+      //console.log("session.url:",session.url )
+      res.redirect(303, session.url);
 
-  //res.send(`Received submission: ${username} (${email})`);
-   console.log("after app.get")
-   console.log("UserName:",username)
-   console.log("Email:",email)
-
-//--- june 19
-
-  //const sessionId = req.body.sessionId;
-  const session = await stripe.checkout.sessions.create({
-    //customer_email:'murraydesnoyer@hotmail.com',    // not substutite this with the email varriable from the HTTP push URL.
-  
-         customer_email:email,
-    line_items: [
-      {
-        
-        // Provide the exact Price ID (for example, price_1234) of the product you want to sell price_1REaqoJz9K1DGkoKdW8pTe0F
-        price: 'price_1REakMJz9K1DGkoKFgvTbLat',
-        quantity: 1,
-      },
-    ],
-    mode: 'payment',
-    // success_url: `${YOUR_DOMAIN}/success.html`,  
-    // cancel_url: `${YOUR_DOMAIN}/cancel.html`,
-    // substute for production  note sessionID will enable us to get infor about the transaction. 
-    //success_url: `${process.env.BASE_URL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
-    //success_url: `${process.env.BASE_URL}/success.html?session_id={CHECKOUT_SESSION_ID}`,
-    //cancel_url: `${process.env.BASE_URL}/cancel.html`,
-    success_url: `${process.env.BASE_URL}/complete?session_id={CHECKOUT_SESSION_ID}`,
-    cancel_url: `${process.env.BASE_URL}/cancel?session_id={CHECKOUT_SESSION_ID}`,
-
-    automatic_tax: {enabled: true},
-  });
-  console.log("Post await stripe checkout")
-  console.log("Email:",email)
-  //console.log(session)
-  //console.log("session.url:",session.url )
-  res.redirect(303, session.url);
-
-},
+    },
  );
 
 // on success return all info about the checkout scession along with the payment info also line items
@@ -162,12 +176,12 @@ app.get(`/complete`, async (req, res)  => {
  // -- stack recall----------------
  const user = userStack.pop(); 
   if (user) { 
-    console.log(`Popped user: ${user.username}, ${user.email}`); 
-    const data1 = {userName: user.username};                      // the template is looking for userName
+    console.log(`Popped user: ${user.username}, ${user.email}, ${user.companyname}`); 
+    const data1 = {userName: user.username, email: user.email, companyname: user.companyname};                      // the template is looking for userName
     console.log(data1);
     // sendEmail(to, subject, template, data)
     sendEmail(user.email, 'Welcome to Origin', 'welcomeMessage',  data1 )
-    sendEmail('sales@originintl.com', 'New ScanViz Customer', 'newScanVizCustomer',data1) 
+    sendEmail('janet.wiaderny@originintl.com', 'New ScanViz Customer', 'newScanVizCustomer',data1) 
   } else { 
     //res.send('Stack is empty'); 
     res.redirect(`${process.env.HUBSPOT_FORM_URL}`)
@@ -186,9 +200,9 @@ app.get(`/cancel`, (req, res) => {
  const user = userStack.pop(); 
   if (user) { 
     //res.send(`Popped user: ${user.name}, ${user.email}`); 
-    console.log(`Popped user: ${user.username}, ${user.email}`); 
-    const data1 = {userName: user.username};                      // the template is looking for userName
-    sendEmail(user.email, 'New ScanViz Customer', 'newScanVizCustomer',data1) 
+    console.log(`Popped user: ${user.username}, ${user.email}, ${user.companyname}`); 
+    const data1 = {userName: user.username, email: user.email, companyname: user.companyname};                      // the template is looking for userName
+    sendEmail(user.email, 'Unsuccessful Payment', 'UnsuccessfulPayment',data1) 
   } else { 
     res.redirect(`${process.env.HUBSPOT_FORM_URL}`)
     //res.send('Stack is empty'); 
@@ -205,4 +219,15 @@ app.get(`/cancel`, (req, res) => {
 //sendEmail('murraydesnoyer@hotmail.com', 'New ScanViz Customer', 'newScanVizCustomer', { userName: 'Don Doe' })
 //sendEmail('email@domain.com', 'Dynamic Email Template with EJS', 'welcomeMessage', { userName:'Don' })
 
-app.listen(4242, () => console.log('http://localhost:4242/submit.html'));
+
+//----------------------- production ------------------
+// using heroku cinfig NODE_ENV to set directives. 
+// NODE_ENV=`production`  or `local`
+// NODE_ENV_TEST=`test`  or `live`  uses sandbox mailtrap and live uses live mailtrap smpt server
+// ---
+const nodeEnv = process.env.NODE_ENV;
+if (nodeEnv === `production`){
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+} else {
+  app.listen(4242, () => console.log('http://localhost:4242/submit.html'));
+}
