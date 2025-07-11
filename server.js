f0ad5a0b74849ac45f204d9f7364129e55a407f7
MurrayDesnoyer <murray.desnoyer@originintl.com>
2025-07-11 16:09:39 -0400

    set to force customer name and email

--- server.js
+++ server.js
@@ -36,10 +36,10 @@ app.get('/submit', async (req, res) => {
           },
         ],
         mode: 'payment',
-        customer_update: {
-          name: 'auto',
-          //email: 'auto',
-        },
+        // customer_update: {
+        //   name: 'auto',
+        //   email: 'auto',
+        // },
         // success_url: `${YOUR_DOMAIN}/success.html`,  
         // cancel_url: `${YOUR_DOMAIN}/cancel.html`,
         // substute for production  note sessionID will enable us to get infor about the transaction. 
